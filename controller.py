249429899bc23f02d33339e6aea685c0a6654315
LETIANTECH98 <qintinyo@gmail.com>
2021-10-09 22:15:26 +0800

    MPMS: Controller

---
+++ controller.py
@@ -0,0 +1,19 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+from test import Ui_MainWindow
+
+class MyMainForm(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(MyMainForm, self).__init__(parent)
+        self.setupUi(self)
+if __name__ == "__main__":
+
+    app = QApplication(sys.argv)
+
+    myWin = MyMainForm()
+
+    myWin.show()
+
+    sys.exit(app.exec_())
