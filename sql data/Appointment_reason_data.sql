b84cbe398e587d65c61b43b0699d954f6a30d6b9
czho0043 <czho0043@student.monash.edu>
2021-10-12 16:18:21 +1100

    appointment reason data in sql

---
+++ sql data/Appointment_reason_data.sql
@@ -0,0 +1,10 @@
+INSERT INTO `team27`.`Appointment_reason` (`Id`, `Reason`) VALUES ('1', 'General inspection');
+INSERT INTO `team27`.`Appointment_reason` (`Id`, `Reason`) VALUES ('2', 'Skin check');
+INSERT INTO `team27`.`Appointment_reason` (`Id`, `Reason`) VALUES ('3', 'Breast check');
+INSERT INTO `team27`.`Appointment_reason` (`Id`, `Reason`) VALUES ('4', 'Fever');
+INSERT INTO `team27`.`Appointment_reason` (`Id`, `Reason`) VALUES ('5', 'Sore muscle');
+INSERT INTO `team27`.`Appointment_reason` (`Id`, `Reason`) VALUES ('6', 'Bone damage');
+INSERT INTO `team27`.`Appointment_reason` (`Id`, `Reason`) VALUES ('7', 'Open wounds');
+INSERT INTO `team27`.`Appointment_reason` (`Id`, `Reason`) VALUES ('8', 'Headache');
+INSERT INTO `team27`.`Appointment_reason` (`Id`, `Reason`) VALUES ('9', 'Cough');
+INSERT INTO `team27`.`Appointment_reason` (`Id`, `Reason`) VALUES ('10', 'Covid vaccine');
