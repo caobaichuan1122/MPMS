05defaf54885d115a84387c9429a5d520fcf5d67
czho0043 <czho0043@student.monash.edu>
2021-10-12 16:19:27 +1100

    gp data in sql

---
+++ sql data/GP_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO `team27`.`GP` (`Id`, `Gname`, `Fname`, `Phone`, `Branch_Id`) VALUES ('1', 'Mike', 'Lee', '4230423', '1');
+INSERT INTO `team27`.`GP` (`Id`, `Gname`, `Fname`, `Phone`, `Branch_Id`) VALUES ('2', 'John', 'Smith', '4240424', '1');
+INSERT INTO `team27`.`GP` (`Id`, `Gname`, `Fname`, `Phone`, `Branch_Id`) VALUES ('3', 'Eva', 'Green', '4250425', '2');
+INSERT INTO `team27`.`GP` (`Id`, `Gname`, `Fname`, `Phone`, `Branch_Id`) VALUES ('4', 'Nina', 'Brown', '4260426', '2');
+INSERT INTO `team27`.`GP` (`Id`, `Gname`, `Fname`, `Phone`, `Branch_Id`) VALUES ('5', 'Giselle', 'Chan', '4270427', '3');
+INSERT INTO `team27`.`GP` (`Id`, `Gname`, `Fname`, `Phone`, `Branch_Id`) VALUES ('6', 'James', 'Levin', '4280428', '3');
