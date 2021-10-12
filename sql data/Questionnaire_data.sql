5aa0717cf1f249b2a8ca2cf32c3b4dcffb493c51
czho0043 <czho0043@student.monash.edu>
2021-10-12 16:20:29 +1100

    questionnaire data in sql

---
+++ sql data/Questionnaire_data.sql
@@ -0,0 +1,4 @@
+INSERT INTO `team27`.`Questionnaire` (`question_id`, `question_content`) VALUES ('1', 'Are you having a fever?');
+INSERT INTO `team27`.`Questionnaire` (`question_id`, `question_content`) VALUES ('2', 'Have you got vaccinated for COVID-19?');
+INSERT INTO `team27`.`Questionnaire` (`question_id`, `question_content`) VALUES ('3', 'Are you experiencing symptoms including runny nose, anosmia(unable to smell) or cough?');
+INSERT INTO `team27`.`Questionnaire` (`question_id`, `question_content`) VALUES ('4', 'Have you been in close contact with someone who was diagnosed with covid-19 in the past week?');
