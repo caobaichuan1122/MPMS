878084b383fd8c8f364e71b3ca1e51e99c7f809f
LETIANTECH98 <qintinyo@gmail.com>
2021-10-14 22:35:59 +0800

    MPMS: Check_in V2 - Tianliang&Chuyu

--- CheckIn.py
+++ CheckIn.py
@@ -1,7 +1,8 @@
 import pymysql
 import datetime
 
-# timedelta 转 time
+
+# timedelta to time
 def convert_timedelta(duration):
     days, seconds = duration.days, duration.seconds
     hours = days * 24 + seconds // 3600
@@ -10,12 +11,52 @@ def convert_timedelta(duration):
     return hours, minutes, seconds
 
 
-def appointment_verify(Id):
+# clear previous check_in record
+def clear_previous():
+    conn = pymysql.connect(host='34.129.105.0',
+                           user='Team27',
+                           password='Team_27_yu',
+                           db='team27',
+                           port=3306,
+                           charset='utf8')
+    cursor = conn.cursor()
+
+    sql = " UPDATE `Appointment` SET `Check_in` = 0 WHERE `App_Date` = current_date() " \
+          "AND `App_Time` < now() - interval 45 minute "
+    cursor.execute(sql)
+    sql_2 = "UPDATE `Appointment` SET `Check_in` = 0 WHERE `App_Date` < current_date()"
+    cursor.execute(sql_2)
+
+
+# print out queue
+def print_queue():
+
+    conn = pymysql.connect(host='34.129.105.0',
+                           user='Team27',
+                           password='Team_27_yu',
+                           db='team27',
+                           port=3306,
+                           charset='utf8')
+    cursor = conn.cursor()
+
+    sql = "SELECT COUNT(*) - 1 FROM `Appointment` WHERE `Check_in` = '1'"
+    cursor.execute(sql)
+    result = cursor.fetchone()
+    # return number of people in front of you
+    num_people = str(result[0])
+    print("Checked in successfully. There are " + num_people +
+          " people in front of you, please wait for our staff to call you.")
+
+
+def appointment_verify(app_id):
+
+    clear_previous()
+
     conn = pymysql.connect(host='34.129.105.0', user='Team27', password='Team_27_yu', db='team27', port=3306,
                            charset='utf8')
     cur = conn.cursor()
 
-    cur.execute('SELECT App_Time, App_Date FROM Appointment WHERE Appointment.Id = %s'%Id)
+    cur.execute('SELECT App_Time, App_Date FROM Appointment WHERE Appointment.Id = %s' % app_id)
 
     result_time, result_date = cur.fetchone()
 
@@ -36,16 +77,16 @@ def appointment_verify(Id):
         diff = check_in.total_seconds() / 60
 
         if diff <= 10:
-
-            cur.execute('UPDATE Appointment SET Check_In = 1 WHERE Appointment.Id = %s'%Id)
-        if diff >= 45:
-            cur.execute('UPDATE Appointment SET Check_In = 0 WHERE Appointment.Id = %s' % Id)
-
+            cur.execute('UPDATE Appointment SET Check_In = 1 WHERE Appointment.Id = %s' % app_id)
+            print_queue()
+        else:
+            print("Failed to check in, please contact staff for instruction!")
         conn.commit()
 
 
-# appointment_verify(1)
 
+# appointment_verify(2)
+# print_queue()
 
 
 
