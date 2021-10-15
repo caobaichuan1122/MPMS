aa28bf62ffc0a8d605919c2116d831238c52ee04
Yujie Sun <ysun0139@student.monash.edu>
2021-10-15 20:44:43 +0800

    complete the comment

--- admin_control.py
+++ admin_control.py
@@ -2,11 +2,13 @@ import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from admin import Admin
 
+# this class is used to control the admin page
 class MyMainForm(QMainWindow, Admin):
     def __init__(self, parent=None):
         super(MyMainForm, self).__init__(parent)
         self.setupUi(self)
 
+# set the structure
 if __name__ == "__main__":
     # s = MainWindow()
     app = QApplication(sys.argv)
